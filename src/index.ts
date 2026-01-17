2986b08668b1df12bec6a167f40d48670196a6c0
haroontalib <haroontalib8029@gmail.com>
2026-01-17 21:15:07 +0500

    installed importent packages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/dbConnection";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -8,7 +10,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.send("Express + TypeScript API running 🚀");
