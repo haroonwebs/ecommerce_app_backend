82d866a925810293c2b0039ef2cdbca0148534ad
haroontalib <haroontalib8029@gmail.com>
2026-01-21 22:37:35 +0500

    created route and its logic for user watch history

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -10,6 +10,7 @@ import {
   updateCoverImage,
   updateAvatarImage,
   getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controllers";
 import { fileUpload } from "../middlewares/multer.middleware";
 import { authenticateUser } from "../middlewares/auth.middleware";
@@ -38,6 +39,7 @@ router.route("/current-user").get(authenticateUser, currentUser);
 router.route("/update-account").put(authenticateUser, updateUserAccount);
 router.route("/update-cover-image").put(authenticateUser, updateCoverImage);
 router.route("/update-avatar-image").put(authenticateUser, updateAvatarImage);
+router.route("/watch-history").get(authenticateUser, getWatchHistory);
 router
   .route("/channel-profile/:username")
   .get(authenticateUser, getUserChannelProfile);
